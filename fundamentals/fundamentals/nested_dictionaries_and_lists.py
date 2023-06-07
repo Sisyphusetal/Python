a2d61e89b097c9467a50b7537152209271c2fb09
Kurt James Clausen <kurtjclausen@gmail.com>
2023-06-06 18:57:30 -0700

    Nested Dictionaries and Lists

--- fundamentals/fundamentals/nested_dictionaries_and_lists.py
+++ fundamentals/fundamentals/nested_dictionaries_and_lists.py
@@ -17,3 +17,48 @@ sports_directory["soccer"][0] = "Andres"
 z[0]['y'] = 30
 print(z)
 
+
+#Iterate through a list of dictionaries
+students = [
+        {'first_name':  'Michael', 'last_name' : 'Jordan'},
+        {'first_name' : 'John', 'last_name' : 'Rosales'},
+        {'first_name' : 'Mark', 'last_name' : 'Guillen'},
+        {'first_name' : 'KB', 'last_name' : 'Tonel'}
+    ]
+def iterateDictionary(some_list):
+    for dict in some_list:
+        print(dict)
+
+iterateDictionary(students)
+
+
+#Get values from a list of dictionaries
+students = [
+        {'first_name':  'Michael', 'last_name' : 'Jordan'},
+        {'first_name' : 'John', 'last_name' : 'Rosales'},
+        {'first_name' : 'Mark', 'last_name' : 'Guillen'},
+        {'first_name' : 'KB', 'last_name' : 'Tonel'}
+    ]
+
+def iterateDictionary2(key_name, some_list):
+    for dict in some_list:
+        print(dict[key_name])
+
+iterateDictionary2("first_name", students)
+iterateDictionary2("last_name", students)
+
+
+#iterate Through a Dictionary with List Values
+dojo = {
+    'locations': ['San Jose', 'Seattle', 'Dallas', 'Chicago', 'Tulsa', 'DC', 'Burbank'],
+    'instructors': ['Michael', 'Amy', 'Eduardo', 'Josh', 'Graham', 'Patrick', 'Minh', 'Devon']
+}
+
+def printInfo(some_dict):
+    for key in some_dict:  # Iterate over keys in the dictionary
+        value = some_dict[key]  # Access the value by key
+        print(f"{key} ({len(value)})")  # Print the key and the size of its list
+        for item in value:  # Iterate over the list
+            print(item)  # Print each item in the list
+
+printInfo(dojo)
